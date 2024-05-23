09350359568abf7d959d5c510e5edba1c5fe367b
IsaacFerreto <ferreto.isaac.com>
2024-05-23 13:19:40 -0600

    FinalCommit

--- gato.js
+++ gato.js
@@ -5,14 +5,27 @@ let puntuacion2 = document.getElementById("puntuacion2");
 let seguridad = true;
 let gateway = 0
 let counter = 0;
-let J1Puntuacion = 0;
-let J2Puntuacion = 0;
+//this is to get the score from local storage
+let J1Puntuacion = JSON.parse(localStorage.getItem('Jugador1'));
+let J2Puntuacion = JSON.parse(localStorage.getItem('Jugador2'));
+
+console.log(J2Puntuacion+' puntiacadsf');
+console.log(J1Puntuacion+' puntutacion');
+
+if (J2Puntuacion==null&&J1Puntuacion==null) {
+  J1Puntuacion=0
+  J2Puntuacion=0
+}
+
+puntuacion1.innerHTML = parseInt(J1Puntuacion);
+puntuacion2.innerHTML = parseInt(J2Puntuacion);
+
 
 
 function human() {
     //Function to set player vs player on the squares
     //This is going to take the functions out so we can put a new one
-
+    alert('Set for 2 Players')
     for (let index = 0; index < cuadro.length; index++) {
         cuadro[index].removeEventListener('click', insertar);
         console.log("Checking Sytem RemoveListener");
@@ -24,6 +37,7 @@ function human() {
     }
 }
 function machine() {
+    alert('Set for 1 Player')
     //Function to set player vs machine on the squares
     //This is going to take the functions out so we can put a new one
     for (let index = 0; index < cuadro.length; index++) {
@@ -31,17 +45,23 @@ function machine() {
         console.log("Checking Sytem RemoveListener");
     }
     //Adding function to each square 
-    for (let index = 0; index < cuadro.length; index++) {
-        cuadro[index].addEventListener('click', insertar)
-        console.log("Checking Sytem MACHINE");
-    }
+    
+        for (let index = 0; index < cuadro.length; index++) {
+            cuadro[index].addEventListener('click', insertar)
+            console.log("Checking Sytem MACHINE");
+        }
+    
+
 }
+
+
+
 
 function insertar(element) {
     let valor = element.target.innerHTML;
     //We are going to check if the square is empty
     if (!valor.length) {
-        element.target.innerHTML = '❌';
+        element.target.innerHTML = 'X';
         jugador1 = !jugador1;
 
         //Counter is used to be checking how many moves had been made to estimated a DRAW
@@ -74,7 +94,7 @@ function Person(element) {
     let valor = element.target.innerHTML;
     if (!valor.length) {
         //tjis terniary  change betwen player one and two
-        element.target.innerHTML = jugador1 ? '❌' : '⭕';
+        element.target.innerHTML = jugador1 ? 'X ' : 'O';
         jugador1 = !jugador1;
         //this counts quantity of moves in case of a tie
         counter++
@@ -123,8 +143,8 @@ function ganador(player) {
     counter = 0
 
     document.getElementById('salida').innerHTML = player + " WIN"
-    alert(player + " WON")
-    alert('game is going to be restarted')
+    
+ 
     for (let index = 0; index < cuadro.length; index++) {
         cuadro[index].innerHTML = ""
     }
@@ -146,7 +166,7 @@ function maquina() {
     if (!valor.length && cuadro[pun].innerHTML == '') {
         
 
-        cuadro[pun].innerHTML = "⭕"
+        cuadro[pun].innerHTML = "O"
         seguridad = !seguridad
     }
 
@@ -179,15 +199,32 @@ function punctuation(a) {
 
     console.log('Puntuation functione running');
     console.log(a + 'aqui deberia estar llegando el emoji');
-    if (a === '❌') {
+    if (a === 'X') {
+        //this is to keep track of the score for O  
         J1Puntuacion++
         puntuacion1.innerHTML = parseInt(J1Puntuacion);
-        console.log('jugador ❌ ' + J1Puntuacion);
+        console.log('jugador X ' + J1Puntuacion);
+        localStorage.setItem('Jugador1', JSON.stringify(J1Puntuacion));
 
     }
-    if (a === '⭕') {
+    //this is to keep track of the score for O
+    if (a === 'O') {
         J2Puntuacion++
         puntuacion2.innerHTML = parseInt(J2Puntuacion);
-        console.log('jugador ⭕ ' + J2Puntuacion);
+        console.log('jugador O ' + J2Puntuacion);
+        localStorage.setItem('Jugador2', JSON.stringify(J2Puntuacion));
     }
 }
+
+
+//This function is going to delete everything fronm local storage
+function restartScore() {
+
+    localStorage.clear();
+    location.reload();
+    
+    console.log(J1Puntuacion+' J1Puntuacion');
+    console.log(J2Puntuacion+' J1Puntuacion');
+
+}
+
